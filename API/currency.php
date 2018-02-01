77284ce1820464879894c6f48a76c668486b9ad3
brijy890 <brijy123@gmail.com>
2018-02-01 22:46:30 +0530

    API

--- API/currency.php
+++ API/currency.php
@@ -3,41 +3,67 @@
 
 <?php
 
-//step1
-$cSession = curl_init(); 
-//step2
-curl_setopt($cSession,CURLOPT_URL,"https://api.fixer.io/latest?base=USD");
-curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($cSession,CURLOPT_HEADER, false); 
-//step3
-$result=curl_exec($cSession);
-//step4
-curl_close($cSession);
-//step5
-echo $result;
-
-exit;
-
-if (isset($_POST['submit'])) {
-	
-	echo $currency = $_POST['currency'];
+if(isset($_POST['submit'])){
+	$currency = $_POST['currency'];
 }
 
+if (empty($currency)) {
+	$currency = 100;
+}
+
+$result = file_get_contents("https://api.fixer.io/latest?base=USD");
+$result = json_decode($result, true);
+$result = $result['rates'];
 
 
+$usd = $result['INR'];
+
+$result = number_format($currency/$usd, 2);
 
 ?>
+
+
 
 
 	<div class="container">
 		<form action="<?php $_SERVER['PHP_SELF'];?>" method="POST">
 
-			<div class="form-group">
-				<input type="text"  placeholder="Enter value in INR" name="currency">
-			</div>
+		<div class="form-group">
+		<input type="text"  placeholder="Enter value in INR" name="currency">
+		</div>
 
-			<input type="submit" value="submit" name="submit" class="btn btn-primary">
-			
+		<input type="submit" value="submit" name="submit" class="btn btn-primary">
+
 		</form>
+		<br>
+		<table class="table table-bordered">
+		<th>INR</th>
+		<th>USD</th>
+		<tbody>
+			<tr>
+				<td><?php echo $currency;?></td>
+				<td><?php echo $result;?></td>
+			</tr>
+		</tbody>
+		</table>
 	</div>
+		
+			
+<?php
+
+// $result = file_get_contents("https://api.fixer.io/latest?base=USD");
+// $result = json_decode($result, true);
+// $result = $result['rates'];
+
+// foreach ($result as $key => $value) {
+
+// 	echo "<tr>";
+// 	echo "<td>$key</td>";
+// 	echo "<td>$value</td>";
+// 	echo "</tr>";
+
+// }
+?>
+
+
 
